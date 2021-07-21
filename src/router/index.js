51ba5c9c0f4d9355f9e7cbd50b2a3806cea643d0
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-20 18:30:48 -0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Test from '../views/Test.vue';
+import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+import Logout from '../views/Logout.vue'
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,12 +14,32 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/test-page',
+    name: 'Test',
+    component: Test
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: Logout
+  },
+  {
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
 ];
 
---
+++ src/views/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logout"></div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created: function () {
+    console.log("in logout");
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.setItem("jwt");
+    this.$router.push("/");
+  },
+};
+</script>
